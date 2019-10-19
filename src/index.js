981687679c42fd1e08ef5bd678a867aa1ff25ac1
artas0000 <anisovich28@mail.ru>
2019-10-19 17:00:42 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    function toSearch() {
+        for (let k = 0; k < newArray.length;) {
+            if (str.indexOf(newArray[k]) === -1) {
+                k++;
+            } else if (str.indexOf(newArray[k]) !== -1) {
+				let itemNumber = str.indexOf(newArray[k]);
+				str = str.slice(0, itemNumber) + str.slice(itemNumber + 2);
+                k = 0;
+            }
+        }
+    }
+
+    let newArray = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let full = bracketsConfig[i][0] + bracketsConfig[i][1];
+        newArray.push(full);
+    }
+
+	
+    toSearch();
+    if (str.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
